0d4c6622d145175183f6f97b727f988dcacdb1c3
Drin Musa <drinmusa99@hotmail.com>
2020-12-08 15:53:02 +0100

    Finished navbar and footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Added javascript");
+// Script for collapsing social icons on top
+
+$(document).ready(function () {
+  $("#social-btn-expand").click(function () {
+    if ($("#social-top").hasClass("showhide")) {
+      $("#social-top").removeClass("showhide");
+      $("#social-icon").removeClass("fa-plus");
+      $("#social-top-content").toggle();
+    } else {
+      $("#social-top").addClass("showhide");
+      $("#social-icon").addClass("fa-plus");
+      $("#social-top-content").toggle();
+    }
+  });
+});
