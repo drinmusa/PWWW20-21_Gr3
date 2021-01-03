8d955e8a854f132707558808efe6af59132d6ea4
Drin Musa <drinmusa99@hotmail.com>
2021-01-03 15:01:22 +0100

    Update slideshow

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ $(document).ready(function () {
     }
   });
 });
+var indexValue = 1;
+showImg(indexValue);
+function btm_slide(e) {
+  showImg((indexValue = e));
+}
+function side_slide(e) {
+  showImg((indexValue += e));
+}
+function showImg(e) {
+  var i;
+  const img = document.querySelectorAll(".slide-image");
+  const slider = document.querySelectorAll(".btm-slides span");
+  if (e > img.length) {
+    indexValue = 1;
+  }
+  if (e < 1) {
+    indexValue = img.length;
+  }
+  for (i = 0; i < img.length; i++) {
+    img[i].style.display = "none";
+  }
+  for (i = 0; i < slider.length; i++) {
+    slider[i].style.background = "rgba(0,0,0,0.1)";
+  }
+  img[indexValue - 1].style.display = "block";
+  slider[indexValue - 1].style.background = "#333";
+}
